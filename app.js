0a8bf9b090d881b33516a38b85702d885a1b63b2
Sebastian-14 <sebastian.sanchez@tecsup.edu.pe>
2020-10-07 16:46:54 -0500

    only peerServer

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-const express = require('express');
 // const fs = require('fs');
-// const { ExpressPeerServer } = require('peer');
-const http = require('http');
-const app = express();
+const { PeerServer } = require('peer');
 
-const server = http.createServer(app);
-
-// app.get('/', (req, res) => {
-//     res.send('hola mundito con covid-19')
-// })
-
-app.get('/prueba', (req, res) => {
-    res.send('hola mundito con covid-19')
-})
-
-
-const PORT = process.env.PORT || 9000
-server.listen(PORT, () => {
-    console.log("Conecte to port:" + PORT)
+const peerServer = PeerServer({
+    path:'/server',
+    port: 9000,
+    // ssl: {
+    //     key: fs.readFileSync('/path/to/your/ssl/key/here.key'),
+    //     cert: fs.readFileSync('/path/to/your/ssl/certificate/here.crt')
+    // }
 });
