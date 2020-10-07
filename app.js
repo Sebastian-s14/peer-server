f9fc1bf2fdf71dd966c73423c533b34ac2fd3b52
Sebastian-14 <sebastian.sanchez@tecsup.edu.pe>
2020-10-07 16:17:13 -0500

    rama prueba

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 const express = require('express');
-const fs = require('fs');
-const { ExpressPeerServer } = require('peer');
+// const fs = require('fs');
+// const { ExpressPeerServer } = require('peer');
 const http = require('http');
 const app = express();
 
 const server = http.createServer(app);
 
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-    path: '/myapp',
-    // ssl: {
-    //     key: fs.readFileSync('/etc/letsencrypt/live/media-server.playtecedu.com/fullchain.pem'),
-    //     cert: fs.readFileSync('/etc/letsencrypt/live/media-server.playtecedu.com/privkey.pem')
-    // }
-});
+// app.get('/', (req, res) => {
+//     res.send('hola mundito con covid-19')
+// })
 
-app.use('/peerjs', peerServer);
-
-app.get('/', (req, res) => {
-    res.send('hola mundito con covid-19')
-})
-
-app.get('/test', (req, res) => {
+app.get('/prueba', (req, res) => {
     res.send('hola mundito con covid-19')
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 9000
 server.listen(PORT, () => {
     console.log("Conecte to port:" + PORT)
 });
